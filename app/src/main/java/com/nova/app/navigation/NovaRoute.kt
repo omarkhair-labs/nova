76a51f560c0e97765f21e64affd3da9a1709040a
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-08 13:02:37 +0300

    Keep Activity within Home navigation shell

--- app/src/main/java/com/nova/app/navigation/NovaRoute.kt
+++ app/src/main/java/com/nova/app/navigation/NovaRoute.kt
@@ -6,7 +6,6 @@ sealed interface NovaRoute {
     data object Login : NovaRoute
     data object ProfileSetup : NovaRoute
     data object Home : NovaRoute
-    data object Notifications : NovaRoute
     data object CreatePost : NovaRoute
     data class PostDetail(val postId: Long) : NovaRoute
     data class PostComments(val postId: Long) : NovaRoute
