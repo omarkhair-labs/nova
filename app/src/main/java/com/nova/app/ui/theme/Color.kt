51c5ba1e9e64f3bb17c0b7b1cdd1b00bc982afc7
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-08 07:17:12 +0300

    Define Nova light design palette

--- app/src/main/java/com/nova/app/ui/theme/Color.kt
+++ app/src/main/java/com/nova/app/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.nova.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NovaBackground = Color(0xFFFAFAF8)
+val NovaSurface = Color(0xFFFFFFFF)
+val NovaInk = Color(0xFF18171C)
+val NovaMuted = Color(0xFF77727F)
+val NovaBorder = Color(0xFFE9E6EE)
+val NovaAccent = Color(0xFF6554E8)
+val NovaAccentSoft = Color(0xFFF0EDFF)
+val NovaDanger = Color(0xFFBF2A3D)
