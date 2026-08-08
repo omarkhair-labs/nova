6459a4cef37a3ca1fadd1ca7a9ee1c2b07fffdf7
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-08 07:30:22 +0300

    Register Nova users in Django admin

---
+++ backend/accounts/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    ordering = ("email",)
+    list_display = ("email", "username", "name", "is_staff", "is_active")
+    search_fields = ("email", "username", "name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Profile", {"fields": ("username", "name", "avatar_url")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "name", "password1", "password2"),
+            },
+        ),
+    )
