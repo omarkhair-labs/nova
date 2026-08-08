ba0eb9a0a73aea5e668a59f9d0975c3e53a278f3
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-08 12:58:59 +0300

    Add Activity route

--- app/src/main/java/com/nova/app/navigation/NovaRoute.kt
+++ app/src/main/java/com/nova/app/navigation/NovaRoute.kt
@@ -6,6 +6,7 @@ sealed interface NovaRoute {
     data object Login : NovaRoute
     data object ProfileSetup : NovaRoute
     data object Home : NovaRoute
+    data object Notifications : NovaRoute
     data object CreatePost : NovaRoute
     data class PostDetail(val postId: Long) : NovaRoute
     data class PostComments(val postId: Long) : NovaRoute
