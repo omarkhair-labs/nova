f90cc5fba3bd93e76331b44ed0bcc93eb02f9172
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-09 06:27:25 +0300

    Add app-wide presence websocket route

--- backend/accounts/routing.py
+++ backend/accounts/routing.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
 
-from .realtime import ConversationConsumer
+from .realtime import ConversationConsumer, PresenceConsumer
 
 
 websocket_urlpatterns = [
+    re_path(r"^ws/presence/$", PresenceConsumer.as_asgi()),
     re_path(
         r"^ws/conversations/(?P<conversation_id>\d+)/$",
         ConversationConsumer.as_asgi(),
