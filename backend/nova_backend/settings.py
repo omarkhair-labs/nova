44e240922e53e25b34f77b94053d933db67c3551
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-09 09:59:29 +0300

    Fix Django template context processor list

--- backend/nova_backend/settings.py
+++ backend/nova_backend/settings.py
@@ -59,11 +59,11 @@ TEMPLATES = [
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
-            "context_processors": {
+            "context_processors": [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            },
+            ],
         },
     },
 ]
