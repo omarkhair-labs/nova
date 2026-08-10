3ad4020ae6c370796be04e207893de90d8ad6030
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-10 13:02:38 +0300

    Expose stories in Django admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from .models import User, UserBlock, UserReport
+from .story_models import Story, StoryReaction, StoryView
 
 
 @admin.register(User)
@@ -54,3 +55,25 @@ class UserBlockAdmin(admin.ModelAdmin):
     list_display = ("blocker", "blocked", "created_at")
     search_fields = ("blocker__username", "blocked__username")
     readonly_fields = ("created_at",)
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "media_type", "created_at", "expires_at")
+    list_filter = ("media_type", "created_at", "expires_at")
+    search_fields = ("author__username", "author__email", "caption")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(StoryView)
+class StoryViewAdmin(admin.ModelAdmin):
+    list_display = ("story", "viewer", "viewed_at")
+    search_fields = ("story__author__username", "viewer__username")
+    readonly_fields = ("viewed_at",)
+
+
+@admin.register(StoryReaction)
+class StoryReactionAdmin(admin.ModelAdmin):
+    list_display = ("story", "user", "emoji", "updated_at")
+    search_fields = ("story__author__username", "user__username", "emoji")
+    readonly_fields = ("created_at", "updated_at")
