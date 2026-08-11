df4d2b63a5dfa1875d822fff1eb53aad61a047ed
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-11 08:45:40 +0300

    Prepare Nova 2.0 version for Play testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.omarkhair70.nova"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
