6f8b677d9477e646aa11da913b8be7e98717fbe1
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-13 11:05:22 +0300

    Add Reels and Messages as primary Nav3 routes

--- app/src/main/java/com/nova/app/navigation/NovaRoute.kt
+++ app/src/main/java/com/nova/app/navigation/NovaRoute.kt
@@ -8,11 +8,13 @@ sealed interface NovaRoute {
     data object Login : NovaRoute
     data object ProfileSetup : NovaRoute
     data object Home : NovaRoute
+    data object People : NovaRoute
+    data object Reels : NovaRoute
+    data object Messages : NovaRoute
+    data object Profile : NovaRoute
     data object CreatePost : NovaRoute
     data class PostDetail(val postId: Long) : NovaRoute
     data class PostComments(val postId: Long) : NovaRoute
-    data object People : NovaRoute
     data class Person(val username: String) : NovaRoute
-    data object Profile : NovaRoute
     data object EditProfile : NovaRoute
 }
