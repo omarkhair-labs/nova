6b83a6e2a8ce3f6f7dd9498704135ca2df70b67b
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-20 02:10:07 +0300

    Phase 3: delete call quality compatibility aliases

--- app/src/main/java/com/nova/app/core/calls/NovaCallAudioQuality.kt
+++
@@ -1,7 +0,0 @@
-package com.nova.app.core.calls
-
-@Deprecated("Use feature/calls WebRTC quality models.")
-typealias NovaCallAudioQualitySnapshot = com.nova.app.feature.calls.webrtc.model.NovaCallAudioQualitySnapshot
-
-@Deprecated("Use feature/calls WebRTC quality models.")
-typealias NovaCallAudioQualityDelta = com.nova.app.feature.calls.webrtc.model.NovaCallAudioQualityDelta
