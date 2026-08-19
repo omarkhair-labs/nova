aac3fca643086736d3a6843bd38ff15a4893daa4
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-20 00:12:13 +0300

    Phase 2: remove obsolete group management alias

--- app/src/main/java/com/nova/app/core/messaging/NovaGroupManagementRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.nova.app.core.messaging
-
-import com.nova.app.feature.messages.group.data.remote.GroupManagementRemoteRepository
-
-
-@Deprecated("Use GroupManagementRepository through AppContainer.")
-typealias NovaGroupManagementRepository = GroupManagementRemoteRepository
