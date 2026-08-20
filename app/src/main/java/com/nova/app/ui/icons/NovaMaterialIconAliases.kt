10fe8d383e8f96c08e4ee33abe415113a277ccbf
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-20 21:45:38 +0300

    Phase 5: move call icon aliases into app-owned namespace

---
+++ app/src/main/java/com/nova/app/ui/icons/NovaMaterialIconAliases.kt
@@ -0,0 +1,17 @@
+package com.nova.app.ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+val Icons.Filled.Mic: ImageVector
+    get() = NovaCommunicationIcons.Mic
+
+val Icons.Filled.VolumeUp: ImageVector
+    get() = NovaCommunicationIcons.VolumeUp
+
+val Icons.Filled.Videocam: ImageVector
+    get() = NovaCommunicationIcons.Video
+
+val Icons.Filled.CallEnd: ImageVector
+    get() = NovaCommunicationIcons.CallEnd
