af8adcccb61fdce27e498897d301f05f2a82acac
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-20 11:58:17 +0300

    Phase 4: bridge legacy social paging model imports

---
+++ app/src/main/java/com/nova/app/core/social/SocialPagingModelCompatibility.kt
@@ -0,0 +1,15 @@
+package com.nova.app.core.social
+
+
+@Deprecated(
+    message = "Use feature/people/domain/model/NovaPersonPage.",
+    replaceWith = ReplaceWith("NovaPersonPage", "com.nova.app.feature.people.domain.model.NovaPersonPage"),
+)
+typealias NovaPersonPage = com.nova.app.feature.people.domain.model.NovaPersonPage
+
+
+@Deprecated(
+    message = "Use feature/people/domain/model/NovaProfilePostPage.",
+    replaceWith = ReplaceWith("NovaProfilePostPage", "com.nova.app.feature.people.domain.model.NovaProfilePostPage"),
+)
+typealias NovaProfilePostPage = com.nova.app.feature.people.domain.model.NovaProfilePostPage
