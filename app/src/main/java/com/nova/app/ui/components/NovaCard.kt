1b66c34034a9ed1c351f638143913f5b2f0ae7fa
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-23 01:17:19 +0300

    Fix NovaCard unit import

--- app/src/main/java/com/nova/app/ui/components/NovaCard.kt
+++ app/src/main/java/com/nova/app/ui/components/NovaCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 import com.nova.app.ui.theme.NovaBorder
 import com.nova.app.ui.theme.NovaSurface
 
