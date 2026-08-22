d93bca2186a5552eb70f64ae6d5692521ab027e2
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-22 20:57:02 +0300

    Release 2.1.5 to Google Play alpha (#195)
    Bump Nova to versionName 2.1.5 / versionCode 20105 for closed testing. Merging app/build.gradle.kts to master triggers the existing Google Play Release workflow on the alpha track.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.omarkhair70.nova"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20104
-        versionName = "2.1.4"
+        versionCode = 20105
+        versionName = "2.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
