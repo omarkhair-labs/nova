1d5bf9e7d5dc530689939760041d2181edbc54f4
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-23 00:21:10 +0300

    Design system: add Nova elevation roles

---
+++ app/src/main/java/com/nova/app/ui/theme/Elevation.kt
@@ -0,0 +1,10 @@
+package com.nova.app.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+/** Shared elevation roles. Prefer borders over shadows unless hierarchy needs lift. */
+object NovaElevation {
+    val flat = 0.dp
+    val raised = 1.dp
+    val floating = 6.dp
+}
