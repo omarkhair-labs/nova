7bb3a05c40f8d69b260f5d6e29952cdb5c349f28
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-23 21:22:54 +0300

    Fix Orbit retry clickable import

--- app/src/main/java/com/nova/app/feature/orbit/OrbitRail.kt
+++ app/src/main/java/com/nova/app/feature/orbit/OrbitRail.kt
@@ -3,6 +3,7 @@ package com.nova.app.feature.orbit
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
